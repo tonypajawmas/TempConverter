cfd788a41b66781b1d105b454d67bb920f197abd
tonypajawmas <91402942+tonypajawmas@users.noreply.github.com>
2022-04-17 15:24:23 -0700

    color-coded

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ document
   .querySelector("#farenToCelButton")
   .addEventListener("click", buttonClick);
 
+let mainBody = document.querySelector("body");
+
 function buttonClick() {
   let getUserInfo = document.querySelector("#farenToCel").value;
   //   console.log(getUserInfo);
@@ -11,6 +13,17 @@ function buttonClick() {
   document.querySelector("#farenToCelDisplay").innerText =
     farenToCelMath.toFixed(1);
   document.querySelector("#reset1").reset();
+  if (getUserInfo <= 32) {
+    mainBody.style.backgroundColor = "blue";
+  } else if (getUserInfo <= 60) {
+    mainBody.style.backgroundColor = "yellow";
+  } else if (getUserInfo <= 85) {
+    mainBody.style.backgroundColor = "orange";
+  } else if (getUserInfo <= 100) {
+    mainBody.style.backgroundColor = "darkgoldenrod";
+  } else {
+    mainBody.style.backgroundColor = "red";
+  }
 }
 
 document
@@ -18,11 +31,22 @@ document
   .addEventListener("click", celToFClick);
 
 function celToFClick() {
-  let getUserInfo = document.querySelector("#celToFaren").value;
-  //   console.log(getUserInfo);
-  let celToFarenMath = getUserInfo * 1.8 + 32;
+  let UserInfo = document.querySelector("#celToFaren").value;
+  //   console.log(UserInfo);
+  let celToFarenMath = UserInfo * 1.8 + 32;
   console.log(celToFarenMath);
   document.querySelector("#celToFarenDisplay").innerText =
     celToFarenMath.toFixed(1);
   document.querySelector("#reset2").reset();
+  if (UserInfo <= 0) {
+    mainBody.style.backgroundColor = "blue";
+  } else if (UserInfo <= 15.6) {
+    mainBody.style.backgroundColor = "yellow";
+  } else if (UserInfo <= 2.4) {
+    mainBody.style.backgroundColor = "orange";
+  } else if (UserInfo <= 37.8) {
+    mainBody.style.backgroundColor = "darkgoldenrod";
+  } else {
+    mainBody.style.backgroundColor = "red";
+  }
 }
